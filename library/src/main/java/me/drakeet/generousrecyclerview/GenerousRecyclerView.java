bf7f8c7adf33484759e6c894a735a18bfb9236f2
drakeet <drakeet.me@gmail.com>
2015-10-23 12:18:39 +0800

    Initial commit

---
+++ library/src/main/java/me/drakeet/generousrecyclerview/GenerousRecyclerView.java
@@ -0,0 +1,61 @@
+package me.drakeet.generousrecyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by drakeet(http://drakeet.me)
+ * Date: 15/10/23 12:11
+ */
+public class GenerousRecyclerView extends RecyclerView {
+
+    int mScrollY = 0;
+    boolean mEating = false;
+
+
+    public GenerousRecyclerView(Context context) {
+        this(context, null);
+    }
+
+
+    public GenerousRecyclerView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+
+    public GenerousRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        addOnScrollListener(new OnScrollListener() {
+            @Override public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                switch (newState) {
+                    case SCROLL_STATE_IDLE:
+                        mEating = false;
+                        break;
+                    case SCROLL_STATE_DRAGGING:
+                        mEating = true;
+                        break;
+                }
+            }
+
+
+            @Override public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                mScrollY += dy;
+            }
+        });
+    }
+
+
+    @Override public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (!mEating && !getClipToPadding() && mScrollY < getPaddingTop()
+                && ev.getY() < getPaddingTop()) {
+            return false;
+        }
+        else {
+            return super.dispatchTouchEvent(ev);
+        }
+    }
+}
